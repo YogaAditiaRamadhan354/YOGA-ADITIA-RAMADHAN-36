f986f18a7922260a882a965b434be3424e1b7f65
YogaditiaR <129285892+YogaAditiaRamadhan354@users.noreply.github.com>
2025-11-28 21:47:56 +0700

    Update index.php

--- X-RPL/VIDEO/PHP/17-55 (RESTORANT)/index.php
+++ X-RPL/VIDEO/PHP/17-55 (RESTORANT)/index.php
@@ -42,6 +42,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
     <div class="container">
         <div class="row">
             <div class="col-md-3">
@@ -104,5 +105,6 @@
             </div>
         </div>
     </div>
+
 </body>
 </html>
