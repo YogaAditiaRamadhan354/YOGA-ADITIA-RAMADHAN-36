1d042eba623b79a799f2ec8d6dfc42913d3f3ae1
YogaditiaR <129285892+YogaAditiaRamadhan354@users.noreply.github.com>
2026-01-12 09:44:30 +0700

    Add files via upload

---
+++ X-RPL/KELAS/Yoga .java/src/Yoga.java
@@ -0,0 +1,38 @@
+
+public class Yoga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka ;
+        angka = 10;
+        System.out.println(angka);
+        System.out.println("angka yang anda inputkan : "+ angka);
+        int angka2;
+        angka2=100;
+        System.out.println("umur saya adalah: "+angka2);
+        
+        double g;
+        g = 9.5;
+        System.out.println(g);
+        
+        float h;
+        h = 3.8f;
+        System.out.println("cici menjual roti "+h);
+        
+        String c;
+        c = "Yoga Aditia";
+        System.out.println(c);
+        
+        char m;
+        m ='@';
+        System.out.println(m);
+        
+        boolean betul = true;
+        boolean salah = false;
+        System.out.println(betul);
+        System.out.println(salah);
+    }
+    
+}
